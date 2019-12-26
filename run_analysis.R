06bbf0b958090bee8553ec6e31cf229da4ac34b4
priyanshu-kumar <priyanshukumar.0309@gmail.com>
2019-12-26 22:50:10 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+library(dplyr)
+
+# read train data and test data
+X_train <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt")
+Sub_train <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
+
+X_test <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+Sub_test <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+
+# read var NAmes
+variable_names <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+
+# read  labels
+labels <- read.table("/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
+
+# Problem 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+Sub_total <- rbind(Sub_train, Sub_test)
+
+# Problem 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_total <- X_total[,selected_var[,1]]
+
+# Problem 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(labels[,2]))
+activitylabel <- Y_total[,-1]
+
+# Problem 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- variable_names[selected_var[,1],2]
+
+# Problem 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_total, activitylabel, Sub_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "/home/alasta/Downloads/Getting-and-Cleaning-Data-Week-4-Assignment-master/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
